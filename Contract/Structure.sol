a3ca05951c5f9dc3ebdd76774ac6b1b23c60fedf
Parbat Lama <parbatlama70@gmail.com>
2023-04-08 23:57:22 +0545

    first commit

---
+++ Contract/Structure.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+contract Structure {
+    struct User {
+        address _id;
+        string fullName;
+        uint citizenshipNumber;
+        uint age;
+        string gender;
+        string dob;
+        string email;
+        string profile;
+        string province;
+        string district;
+        string municipality;
+        string ward;
+    }
+
+    struct ReplyComment {
+        address userId;
+        string replyMsg;
+        string createdAt;
+    }
+
+    struct Voter {
+        User user;
+        address[] votedCandidateList;
+        uint voteLimitCount;
+    }
+    struct Candidate {
+        User user;
+        string partyName;
+        string agenda;
+        uint voteCount;
+        address[] votedVoterLists;
+    }
+
+    struct Party{
+        address owner;
+        string name;
+        uint totalMember;
+        string agenda;
+        string logoUrl;
+        address[] members;
+    }
+
+    struct Election{
+        string title;
+        string description;
+        string startDate;
+        string endDate;
+        string electionType;
+        address[] selectedCandidates;
+        string[] galleryImagesUrl;
+    }
+
+    struct FAQ {
+        address _id;
+        string title;
+        string description;
+        string fileUrl;
+        string createdAt;
+        ReplyComment[] comments;
+    }
+}
